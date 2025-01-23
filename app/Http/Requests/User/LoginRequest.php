ee4392d6e2780ed0ce08f65cee043ae6486bde2a
Wassim_Hallack <wassim.hallack@outlook.com>
2025-01-23 14:43:43 +0300

    3

--- app/Http/Requests/User/LoginRequest.php
+++ app/Http/Requests/User/LoginRequest.php
@@ -9,24 +9,24 @@ use Illuminate\Validation\ValidationException;
 
 class LoginRequest extends FormRequest
 {
-/
-* Determine if the user is authorized to make this request.
-*/
+    /**
+     * Determine if the user is authorized to make this request.
+     */
     public function authorize(): bool
     {
         return true;
     }
 
-/
-* Get the validation rules that apply to the request.
-*
-* @return array<string, ValidationRule|array|string>
-*/
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
     public function rules(): array
     {
         return [
             'email' => 'required|email',
-            'password' => 'required|min:6'
+            'password' => 'required|min:6',
         ];
     }
 
